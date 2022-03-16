f512b4530ada15b1ccccdb4153b9cdd66122fbc8
Emmanuel Odonkor <emmadonk@gmail.com>
2022-03-16 12:11:04 +0000

    second commit

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>My name is Emmanuel Odonkor</p>
+        <p>My name is Emmanuel Teye-Kofi Odonkor</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
